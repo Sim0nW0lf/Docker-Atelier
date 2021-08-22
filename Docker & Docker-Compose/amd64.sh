a8f18280783b3172380e3c7df233f4eaa56113b9
Sim0nW0lf <31454341+Sim0nW0lf@users.noreply.github.com>
2021-08-22 13:52:28 +0200

    add amd64 script

---
+++ Docker & Docker-Compose/amd64.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#
+#Update
+#
+apt -qq update
+apt -qq upgrade -y
+
+#
+#ínstall Docker
+#
+apt-get -qq install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get -qq update
+
+apt-get -qq install docker-ce docker-ce-cli containerd.io -y
+
+#
+#install docker-compose
+#
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+apt -qq autoremove -y
