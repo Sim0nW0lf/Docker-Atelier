b88b71af744c9f4d15b5eea4ce9897ae4e7372fc
Sim0nW0lf <31454341+Sim0nW0lf@users.noreply.github.com>
2021-08-23 11:56:28 +0200

    Update amd64.sh

--- 1. Docker & Docker-Compose/amd64.sh
+++ 1. Docker & Docker-Compose/amd64.sh
@@ -19,12 +19,12 @@ apt-get -qq install \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get -qq update
 
-apt-get -qq install docker-ce docker-ce-cli containerd.io -y
+apt-get install docker-ce docker-ce-cli containerd.io
 
 #
 #install docker-compose
