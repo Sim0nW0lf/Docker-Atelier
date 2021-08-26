b0f7c2bb3da6d79a2d05e5d2e7d9133daf73ed02
Sim0nW0lf <31454341+Sim0nW0lf@users.noreply.github.com>
2021-08-27 01:43:32 +0200

    automate everything

--- 2. Traefik/_Installation/setup.sh
+++ 2. Traefik/_Installation/setup.sh
@@ -13,49 +13,42 @@ echo ""
 echo "Installing htpasswd to generate a password"
 echo ""
 sudo apt-get -qq update
+sudo apt-get -qq upgrade -y
 sudo apt-get -qq install apache2-utils -y
 
 #creating acme.json
 touch ../Container-Data/data/acme.json
 chmod 600 ../Container-Data/data/acme.json
 
-#
-#Generate User:Password
-#
-#get user:password string
-u=$(cat ../docker-compose.yml | grep '"traefik.http.middlewares.traefik-auth.basicauth.users=')
-u="$(grep -oP '(?<=users=).*?(?=")' <<< "$u")"
-#we need user:password later, so continue with new variable
-p=$u
-#replacing : with a space
-p=${p/:/ }
-
-echo "Generating htpasswd from user:password credentials"
-echo ""
-p=$(echo $(sudo htpasswd -nb $p) | sed -e s/\\$/\\$\\$/g)
-
-echo "Replacing user:password in docker-compose.yml"
-echo ""
-sed -i "s,$u,$p," ../docker-compose.yml
+echo "Enter your Traefik domain. (Something like traefik.serverdomain.com)"
+read domain
+sed -i "s|$(cat ../docker-compose.yml | grep 'traefik.http.routers.traefik.rule=Host')|      - \"traefik.http.routers.traefik.rule=Host(\`${domain}\`)\"  #your Traefik domain (Something like traefik.serverdomain.com)|" ../docker-compose.yml
+sed -i "s|$(cat ../docker-compose.yml | grep 'traefik.http.routers.traefik-secure.rule=Host')|      - \"traefik.http.routers.traefik-secure.rule=Host(\`${domain}\`)\"  #your Traefik domain (Something like traefik.serverdomain.com)|" ../docker-compose.yml
 
 echo "Enter you E-Mail here:"
 read email
-sed -i "s,example-email@mymail.com,$email," ../Container-Data/data/traefik.yml
+sed -i "s|$(cat ../Container-Data/data/traefik.yml | grep '      email:')|      email: ${email}  ###|" ../Container-Data/data/traefik.yml
 
-#create proxy network
+#
+#Generate User:Password
+#
+echo "Enter your Traefik webinterface username:"
+read user
+echo "Enter your Traefik webinterface password:"
+read password
+traefik_credentials=$(echo $(sudo htpasswd -nb $user $password) | sed -e s/\\$/\\$\\$/g)
+sed -i "s|$(cat ../docker-compose.yml | grep 'traefik.http.middlewares.traefik-auth.basicauth.users')|      - \"traefik.http.middlewares.traefik-auth.basicauth.users=${traefik_credentials}\"  #USER:PASSWORD (Password generated with htpasswd. See here: https://mindup.medium.com/add-basic-authentication-in-docker-compose-files-with-traefik-34c781234970)|" ../docker-compose.yml
+
 docker network create proxy
 
 echo "Launching Traefik now"
 echo ""
 sudo docker-compose -f "../docker-compose.yml" up -d
-
+sleep 5
 echo ""
 echo "*****************************************************"
 echo "*                                                   *"
 echo "*                 Congratulations!                  *"
 echo "*            Traefik is now installed.              *"
-echo "*                                                   *"
-echo "* PLEASE WAIT A MINUTE                              *"
-echo "* for traefik to make the web interface accessible  *"
 echo "*                                                   *"
 echo "*****************************************************"
