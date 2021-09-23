e82d6bc09dedcfeb22316ab8a9f7e34c97cfc175
Sim0nW0lf <31454341+Sim0nW0lf@users.noreply.github.com>
2021-09-23 22:50:47 +0200

    log updates

--- Nextcloud/update.sh
+++ Nextcloud/update.sh
@@ -4,7 +4,9 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR
 
+#log updates
+{ echo $'\n' ; echo $'Update Date:' ; date ; echo '' ; } >> ./update.log
 #update Nextcloud
-docker-compose stop
-docker-compose build --pull
-docker-compose up -d
+docker-compose stop >> ./update.log
+docker-compose build --pull >> ./update.log
+docker-compose up -d >> ./update.log
